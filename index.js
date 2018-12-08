d04d7129e10e99ddd2a6258b510122afee2a0464
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-12-08 12:53:21 -0500

    required both the routes into the index file and set the server to use them both

--- index.js
+++ index.js
@@ -3,8 +3,21 @@ const cors = require('cors');
 
 const server = express();
 
+const projectRouter = require('./routes/projectRouter')
+const actionRouter = require('./routes/actionRouter')
+
 const PORT = process.env.PORT || 5050;
 
+server.use(
+    cors(),
+    express.json()
+    )
+
+  //CRUD endpoint for the projects
+server.use('/api/projects', projectRouter);
+
+  //CRUD endpoints for the actions
+server.use('/api/actions', actionRouter);
 
 server.listen(PORT, () => {
   console.log(`express server is running on port ${PORT}`)
